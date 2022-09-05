52ed743b1c8b767dc51e1311f80a1cec6037a424
Omar Aflak <aflakomar@gmail.com>
2022-09-05 19:56:45 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='quadtree',
+    version='1.0',
+    description='QuadTree library',
+    author='Omar Aflak',
+    author_email='aflakomar@gmail.com',
+    packages=['quadtree'],
+    install_requires=['wheel']
+)
